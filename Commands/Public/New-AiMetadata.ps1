95c8675501e26f9ef2e10ce4d7c31cec8e754695
Jiri Formacek <jiri.formacek@greycorbel.com>
2025-03-02 10:21:14 +0100

    simplified metric and metadata initialization

--- Commands/Public/New-AiMetadata.ps1
+++ Commands/Public/New-AiMetadata.ps1
@@ -4,8 +4,22 @@ Function New-AiMetadata
     .SYNOPSIS
         Creates new metadata collection to be used with Add-AiMetadata
     #>
+    param 
+    (
+        [Parameter()]
+        [string]
+            #Name of the metric
+        $Name,
+        [Parameter()]
+        [string]
+            #Value of the metric
+        $Value
+    )
+
     Process
     {
-        new-object 'System.Collections.Generic.Dictionary[String,String]'
+        $metadata = new-object 'System.Collections.Generic.Dictionary[String,String]'
+        if(-not [string]::IsNullOrEmpty($Name)) {$metadata[$Name] = $Value}
+        return $metadata
     }
 }
--- Commands/Public/New-AiMetric.ps1
+++ Commands/Public/New-AiMetric.ps1
@@ -4,8 +4,21 @@ Function New-AiMetric
     .SYNOPSIS
         Creates new metrics collection to be used with Add-AiMetric
     #>
+    param 
+    (
+        [Parameter()]
+        [string]
+            #Name of the metric
+        $Name,
+        [Parameter()]
+        [double]
+            #Value of the metric
+        $Value
+    )
     Process
     {
-        new-object 'System.Collections.Generic.Dictionary[String,Double]'
+        $metric = new-object 'System.Collections.Generic.Dictionary[String,Double]'
+        if(-not [string]::IsNullOrEmpty($Name)) {$metric[$Name] = $Value}
+        return $metric
     }
 }
